e8710f4ea2adeb73e7a43685519ca31eb4fa12f5
zhangyangfei <674557629@qq.com>
2019-12-19 22:31:57 +0800

    servlet

---
+++ servlet-learn/src/com/zyf/servlet/ServletDemo.java
@@ -0,0 +1,32 @@
+package com.zyf.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 访问：http://localhost:8080/servlet-learn/servletdemo
+//@WebServlet(description = "servlet学习1", urlPatterns = { "/servletdemo" }) // url匹配放在web.xml中
+public class ServletDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletDemo() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("....I am ServletDemo");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ servlet-learn/src/com/zyf/servlet/ServletDemo2.java
@@ -0,0 +1,32 @@
+package com.zyf.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// ∑√Œ £∫http://localhost:8080/servlet-learn/servletdemo2
+@WebServlet(description = "servlet—ßœ∞2", urlPatterns = { "/servletdemo2" })
+public class ServletDemo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletDemo2() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println(" I am ServlerDemo2");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
