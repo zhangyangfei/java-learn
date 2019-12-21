d43556060183edd4ef4a75ed36e59f03bd7558f4
zhangyangfei <674557629@qq.com>
2019-12-21 22:27:05 +0800

    network

---
+++ java-base/src/main/java/com/zyf/javabase/network/NetWorkTest.java
@@ -0,0 +1,21 @@
+package com.zyf.javabase.network;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NetWorkTest {
+
+	// ip对象测试
+	public void inetAddressTest() throws IOException {
+		InetAddress inetAddress = InetAddress.getByName("www.baidu.com");
+		System.out.println("是否可以访问：" + inetAddress.isReachable(2000));
+		System.out.println("ip：" + inetAddress.getHostAddress());
+		
+		InetAddress localAddress= InetAddress.getByAddress(new byte[] {127,0,0,1});
+		System.out.println("是否可以访问：" + localAddress.isReachable(2000));
+		System.out.println("ip：" + localAddress.getHostAddress());
+	}
+}
--- java-base/src/test/java/com/zyf/javabase/JavaBaseApplicationTests.java
+++ java-base/src/test/java/com/zyf/javabase/JavaBaseApplicationTests.java
@@ -1,16 +1,29 @@
 package com.zyf.javabase;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.zyf.javabase.network.NetWorkTest;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JavaBaseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	NetWorkTest netWorkTest;
 
+	@Test
+	public void test1() {
+		try {
+			netWorkTest.inetAddressTest();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
