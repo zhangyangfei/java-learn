013ac22aa97acbceb7be33a2ed237a935046e381
zhangyangfei <674557629@qq.com>
2020-09-30 21:06:39 +0800

    idea提交代码

--- java-base/src/main/java/com/zyf/javabase/JavaBaseApplication.java
+++ java-base/src/main/java/com/zyf/javabase/JavaBaseApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaBaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+        System.out.println("java-base工程启动成功！");
+    }
 
 }
