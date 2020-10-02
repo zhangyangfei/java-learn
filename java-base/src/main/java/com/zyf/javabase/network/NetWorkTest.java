dfa8167915868a04ba36fd928ad36b3c293876ec
zhangyangfei <674557629@qq.com>
2020-10-02 10:14:45 +0800

    网络编程

--- java-base/src/main/java/com/zyf/javabase/network/NetWorkTest.java
+++ java-base/src/main/java/com/zyf/javabase/network/NetWorkTest.java
@@ -5,10 +5,13 @@ import java.net.InetAddress;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * 网络编程
+ */
 @Service
 public class NetWorkTest {
 
-	// ip对象测试
+    //ip对象测试
 	public void inetAddressTest() throws IOException {
 		InetAddress inetAddress = InetAddress.getByName("www.baidu.com");
 		System.out.println("是否可以访问：" + inetAddress.isReachable(2000));
