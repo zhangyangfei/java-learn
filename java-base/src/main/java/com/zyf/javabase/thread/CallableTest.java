d9e0876dc52088258b20747ef586d15e144b42a4
zhangyangfei <674557629@qq.com>
2020-10-05 23:17:40 +0800

    实现Callable接口创建线程

---
+++ java-base/src/main/java/com/zyf/javabase/thread/CallableTest.java
@@ -0,0 +1,39 @@
+package com.zyf.javabase.thread;
+
+import java.util.concurrent.*;
+
+/**
+ * Callable创建线程
+ */
+public class CallableTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        // 创建线程，返回结果
+        FutureTask<Integer> ft = new FutureTask<>(new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "执行call方法...");
+                return 1;
+            }
+        });
+
+        Thread t = new Thread(ft, "t线程");
+        t.start();
+
+        // 判断任务是否完成
+        boolean done = ft.isDone();
+        System.out.println(t.getName() + "是否结束：" + done);
+
+        // 获取线程结果，get()是个阻塞主线程运行的方法
+//        Integer result = ft.get();
+        // 获取线程结果，get(long timeout, TimeUnit unit)，重载方法，超时则抛出异常
+        Integer result = ft.get(1999, TimeUnit.MILLISECONDS);
+        System.out.println(t.getName() + "是否结束：" + ft.isDone());
+        System.out.println(t.getName() + "执行结果：" + result);
+    }
+}
